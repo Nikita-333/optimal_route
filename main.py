3b30e2c5e87d7a29cb1df5f59df4b11b51f8b801
Nikita-333 <80544184+Nikita-333@users.noreply.github.com>
2023-08-22 19:15:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,38 +3,10 @@ from sklearn.cluster import KMeans
 import numpy as np
 from flask import Flask
 from sklearn.metrics import silhouette_score
+
+from Route import Route
+
 app = Flask(__name__)
-
-class Route:
-    def __init__(self, pickup_coordinates, delivery_coordinates):
-        self.pickup_coordinates = pickup_coordinates
-        self.delivery_coordinates = delivery_coordinates
-        self.distance_matrix = self.calculate_distance_matrix()
-
-#метод вычисления расстояния "по точкам"
-    def calculate_distance(self, coord1, coord2):
-        x_a, y_a = coord1
-        x_b, y_b = coord2
-        distance = math.sqrt(pow(x_b - x_a, 2) + pow(y_b - y_a, 2))
-        return distance
-
-#метод для вычисления матрицы расстояния
-    def calculate_distance_matrix(self):
-        num_pickups = len(self.pickup_coordinates)
-        num_deliveries = len(self.delivery_coordinates)
-        matrix = np.zeros((num_pickups, num_deliveries))
-
-        for i in range(num_pickups):
-            for j in range(num_deliveries):
-                matrix[i, j] = self.calculate_distance(
-                    self.pickup_coordinates[i], self.delivery_coordinates[j])
-        return matrix
-
-    def cluster_delivery_points(self, num_clusters):
-        kmeans = KMeans(n_clusters=num_clusters)
-        kmeans.fit(self.distance_matrix)
-        return kmeans.labels_
-
 
 @app.route("/")
 @app.route("/info_route")
@@ -44,8 +16,10 @@ def info_route():
 
     route = Route(pickup_coords, delivery_coords)
 
-    num_clusters = 2
-    cluster_labels = route.cluster_delivery_points(num_clusters)
+    eps = 0.5
+    min_samples = 2
+
+    cluster_labels = route.cluster_delivery_points(eps, min_samples)
 
     return f"Забор: {route.pickup_coordinates}, Доставка: {route.delivery_coordinates}, " \
            f"Матрица расстояний: {route.distance_matrix}, Кластеры: {cluster_labels}"
